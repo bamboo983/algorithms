2082739f78174df2964b9f93ef451a862bbc7370
Bamboo Ku <bamboo983@gmail.com>
2016-11-10 17:26:30 +0800

    Use maximum size at most k
    Shuffle the input strings and enqueue the first k strings.

--- queues/Subset.java
+++ queues/Subset.java
@@ -1,12 +1,17 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Subset {
     public static void main(String[] args) {
         RandomizedQueue<String> q = new RandomizedQueue<String>();
-        while (!StdIn.isEmpty())
-            q.enqueue(StdIn.readString());
-        for (int i = 0; i < Integer.parseInt(args[0]); i++)
+        String[] input = StdIn.readAllStrings();
+        int k = Integer.parseInt(args[0]);
+
+        StdRandom.shuffle(input);
+        for (int i = 0; i < k; i++)
+            q.enqueue(input[i]);
+        for (int i = 0; i < k; i++)
             StdOut.println(q.dequeue());
     }
 }
